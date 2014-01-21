2cda6796705470d3552ed792e03953da4edb2b31
trta <honza.trtik@gmail.com>
2014-01-21 21:39:10 +0100

    Fixtures, model entity pro item / category

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 
 			new Muzar\ScraperBundle\MuzarScraperBundle(),
             new Muzar\ProductCatalogBundle\MuzarProductCatalogBundle(),
+            new Muzar\BazaarBundle\MuzarBazaarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -26,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
