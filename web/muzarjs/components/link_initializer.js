a92bbba6f54b8a45124814f5c0898f07d42c89cc
trta <honza.trtik@gmail.com>
2014-03-05 17:08:34 +0100

    upravy linku v js

--- web/muzarjs/components/link_initializer.js
+++ web/muzarjs/components/link_initializer.js
@@ -9,39 +9,10 @@ define([
 		can.Component.extend({
 			tag: 'a',
 			scope: {
-				state: state,
-				'data-route': '@',
-				'data-route-remove': '@'
+				state: state
 			},
 			events: {
 				click: function(element, event) {
-
-					var route = element.data('route');
-					var remove = element.data('routeRemove') || false;
-
-					if (route) {
-
-						event.preventDefault();
-						event.stopPropagation();
-
-						var params = can.route.deparam(route);
-
-						can.batch.start();
-
-						// Prepiseme route params
-						var state = this.scope.state;
-						state.attr(params, remove);
-
-						// Vsechny prazdne params z routy vyhodime
-						can.each(params, function(value, key) {
-							if (!value) {
-								state.removeAttr(key);
-							}
-						});
-
-						can.batch.stop();
-					}
-
 				}
 			}
 
