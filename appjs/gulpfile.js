efcf0257c6586325ad2af1c884d6908ba5ff0afd
trta <honza.trtik@gmail.com>
2015-01-23 11:11:57 +0100

    gulpfile build

---
+++ appjs/gulpfile.js
@@ -0,0 +1,71 @@
+var _ = require('lodash');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var util = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglifyjs');
+var streamify = require('gulp-streamify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+
+
+
+var bundlesConfig = [{
+    name: 'test',
+    files: ['./test.js'],
+    dest: './../web/build/'
+}];
+
+
+
+gulp.task('browserify', function(){
+    _.each(bundlesConfig, function(config) {
+        browserifyShare(config);
+    });
+});
+
+
+gulp.task('watch', function() {
+    _.each(bundlesConfig, function(config) {
+        browserifyShare(config, true);
+    });
+});
+
+
+function browserifyShare(config, watch){
+
+    var b = browserify(config.files, {
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    });
+    b.transform(reactify);
+
+    if (watch) {
+        b = watchify(b);
+        b.on('update', function(){
+            util.log('Updating...');
+            bundleShare(b, config);
+            util.log(util.colors.green('Done!'));
+        });
+    }
+
+    return bundleShare(b, config);
+}
+
+function bundleShare(b, config) {
+    return b.bundle()
+        .on('error', function(e) {
+            util.log(util.colors.red('Browserify error'), e.message);
+        })
+        .pipe(source(config.name))
+        .pipe(streamify(uglify()))
+        .pipe(rename(function(path) {
+            path.extname = '.min.js';
+        }))
+        .pipe(gulp.dest(config.dest));
+}
+
---
+++ appjs/test.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+var React = require('react');
+
+
+
+//var TestComponent = React.createClass({
+//    render: function() {
+//        return (
+//            <h1>test</h1>
+//        );
+//    }
+//});
+//
+//React.render(
+//    <TesComponent />,
+//    document.getElementById('body')
+//);
+
+
+
+
+var a = 4;
