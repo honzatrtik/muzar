79b77d0a3ab3f900b81d8d17100ea9955d3c5098
trta <honza.trtik@gmail.com>
2015-05-15 14:45:22 +0200

    test pre commit

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./data/bin/phpunit -c ./data/app/phpunit.xml.dist && cd ./data/appjs && npm test && npm run apitest
+phpunit -c ./data/app/phpunit.xml.dist && cd ./data/appjs && npm test && npm run apitest
